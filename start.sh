383368d341c97b18b0fad7980326fd8e4b9f90ed
CABellamy <chrismbellamy@gmail.com>
2018-01-19 10:35:23 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,3 @@
 docker kill basicwebapp && docker rm basicwebapp
 docker build -t basicwebapp .
 docker run -it -d --rm --name basicwebapp -p 8000:8000 basicwebapp:latest
-java -Dwebdriver.chrome.driver=chromedriver -jar selenium-server.jar
